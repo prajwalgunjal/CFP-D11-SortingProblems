d606e5ec76b2135da261b7c40878eae9146ce4c5
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-06-18 22:54:38 +0530

    Insertionsort added

---
+++ InsertionSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingProblem
+{
+    internal class InsertionSort
+    {
+        public void sort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 1; i < n; ++i)
+            {
+                int key = arr[i];
+                int j = i - 1;
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Solving by Insertion sort");
+            for (int i=0; i < arr.Length; ++i)
+            {
+                Console.Write(arr[i]+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@
         {
             int[] arr = { 2, 3, 5, 1, 6 };
             BubbleSort.Bubblesort(arr);
+
+            InsertionSort insertionSort = new InsertionSort();
+            insertionSort.sort(arr);
          }
     }
 }
